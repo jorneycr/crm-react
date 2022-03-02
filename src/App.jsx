c2d5cd90d5c3e2ff0661a3454bcaa631216c13f2
jorneylm <jorney95@live.com>
2022-03-01 22:21:09 -0600

    ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,20 @@
 import { useState } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import IniciarSesion from './layout/IniciarSesion'
 import Layout from './layout/Layout'
 import Inicio from './pages/Inicio'
-import LoginForm from './pages/LoginForm'
+import NuevoCliente from './pages/NuevoCliente'
+import EditarCliente from './pages/EditarCliente'
+
+
 function App() {
 
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<IniciarSesion />}>
-          <Route index element={<LoginForm />} />
-        </Route>
         <Route path='/clientes' element={<Layout />}>
           <Route index element={<Inicio />} />
+          <Route path='nuevo' element={<NuevoCliente />} />
+          <Route path='editar/:id' element={<EditarCliente />} />
         </Route>
       </Routes>
     </BrowserRouter>
--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,8 +1,13 @@
 import { Outlet } from 'react-router-dom'
 const Layout = () => {
     return (
-        <div>
-            <p>Layout</p>
+        <div className='md:flex md:min-h-screen'>
+            <div className='md:w-1/4'>
+                1
+            </div>
+            <div className='md:w-3/4'>
+                2
+            </div>
             <Outlet />
         </div>
 
