45bf7f003d235ad2b746a7db7c698b2269b0372b
jorneylm <jorney95@live.com>
2022-03-03 18:49:02 -0600

    ui layout

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,17 +1,31 @@
-import { Outlet } from 'react-router-dom'
+import { Outlet } from "react-router-dom";
 const Layout = () => {
-    return (
-        <div className='md:flex md:min-h-screen'>
-            <div className='md:w-1/4'>
-                1
-            </div>
-            <div className='md:w-3/4'>
-                2
-            </div>
-            <Outlet />
-        </div>
+  return (
+    <div className="md:flex md:min-h-screen">
+      <div className="md:w-1/4 bg-blue-900 px-5 py-10">
+        <h2 className="text-4xl font-black text-center text-white">
+          CRM - Clientes
+        </h2>
+        <nav className="mt-10">
+          <a
+            href="/clientes"
+            className="text-white text-2xl block mt-2 hover:text-blue-300"
+          >
+            Clientes
+          </a>
+          <a
+            href="/clientes/nuevo"
+            className="text-white text-2xl block mt-2 hover:text-blue-300"
+          >
+            Nuevos Clientes
+          </a>
+        </nav>
+      </div>
+      <div className="md:w-3/4">
+        <Outlet />
+      </div>
+    </div>
+  );
+};
 
-    )
-}
-
-export default Layout
+export default Layout;
